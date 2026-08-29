39a844a4d9c609b3eed7c101b8df29bd290aea6a
eduardommb <lemb@cesar.school>
2026-08-29 19:27:43 -0300

    feat: pthreads 1

--- src/main.c
+++ src/main.c
@@ -2,7 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
+#include <pthread.h>
 
+typedef struct
+{
+    unsigned char *imagem;
+    int largura;
+    int altura;
+    int max_iteracoes;
+    int y_inicio;
+    int y_fim;
+} ThreadBlock;
+
+unsigned char calcular_pixel (int x, int y, int largura, int altura, int max_iteracoes)
+{
+    /* converte pixel p/ plano complexo */
+    double cr = -2.0 + ((double)x / largura) * 3.0;
+    double ci = -1.5 + ((double)y / altura) * 3.0;
+
+    double zr = 0.0;
+    double zi = 0.0;
+    int iter = 0;
+
+    while (iter < max_iteracoes && (zr * zr + zi * zi) <= 4.0)
+    {
+        double zr_temp = zr * zr - zi * zi + cr;
+        zi = 2.0 * zr * zi + ci;
+        zr = zr_temp;
+        iter++;
+    }
+
+    return (unsigned char)(((double)iter / max_iteracoes) * 255.0);
+}
+
+// TEMPO E ARQUIVO
 double pegaTempo(void)
 {
     struct timespec ts;
@@ -54,27 +87,7 @@ int salvarImagem(const char *nome_arquivo, unsigned char *imagem, int largura, i
     return 1;
 }
 
-unsigned char calcular_pixel (int x, int y, int largura, int altura, int max_iteracoes)
-{
-    /* converte pixel p/ plano complexo */
-    double cr = -2.0 + ((double)x / largura) * 3.0;
-    double ci = -1.5 + ((double)y / altura) * 3.0;
-
-    double zr = 0.0;
-    double zi = 0.0;
-    int iter = 0;
-
-    while (iter < max_iteracoes && (zr * zr + zi * zi) <= 4.0)
-    {
-        double zr_temp = zr * zr - zi * zi + cr;
-        zi = 2.0 * zr * zi + ci;
-        zr = zr_temp;
-        iter++;
-    }
-
-    return (unsigned char)(((double)iter / max_iteracoes) * 255.0);
-}
-
+// SERIAL
 void serial(unsigned char *imagem, int largura, int altura, int max_iteracoes)
 {
     for (int y = 0; y < altura; y++)
@@ -86,6 +99,7 @@ void serial(unsigned char *imagem, int largura, int altura, int max_iteracoes)
     }
 }
 
+// OPENMP
 void openmp(unsigned char *imagem, int largura, int altura, int max_iteracoes, int num_threads)
 {
     // define o numero de threads
@@ -102,6 +116,71 @@ void openmp(unsigned char *imagem, int largura, int altura, int max_iteracoes, i
     }
 }
 
+// PTHREADS 1
+
+void *worker_pthreads1(void *arg) {
+    ThreadBlock *dados = (ThreadBlock *)arg;
+
+    for (int y = dados->y_inicio; y < dados->y_fim; y++) {
+        for (int x = 0; x < dados->largura; x++) {
+            dados->imagem[y * dados->largura + x] = calcular_pixel(x, y, dados->largura, dados->altura, dados->max_iteracoes);
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+int pthreads1(unsigned char *imagem, int largura, int altura, int max_iteracoes, int num_threads)
+{
+    pthread_t *threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
+    ThreadBlock *args = (ThreadBlock *)malloc(num_threads * sizeof(ThreadBlock));
+
+    if (threads == NULL || args == NULL) {
+        fprintf(stderr, "Erro: Falha na alocacao de memoria para Pthreads 1.\n");
+        free(threads);
+        free(args);
+        return 0;
+    }
+
+    int linhas_por_thread = altura / num_threads;
+    int resto = altura % num_threads;
+    int linha_atual = 0;
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        args[i].imagem = imagem;
+        args[i].largura = largura;
+        args[i].altura = altura;
+        args[i].max_iteracoes = max_iteracoes;
+        args[i].y_inicio = linha_atual;
+
+        /* distribui o resto da divisao para a ultima thread cobrir toda a imagem */
+        int linhas_dessa_thread = linhas_por_thread + (i == num_threads - 1 ? resto : 0);
+        args[i].y_fim = linha_atual + linhas_dessa_thread;
+        linha_atual = args[i].y_fim;
+
+        if (pthread_create(&threads[i], NULL, worker_pthreads1, (void *)&args[i]) != 0)
+        {
+            fprintf(stderr, "Erro: Falha ao criar a thread %d no Pthreads 1.\n", i);
+            free(threads);
+            free(args);
+            return 0;
+        }
+    }
+
+    /* aguarda todas as threads terminarem */
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+    free(threads);
+    free(args);
+    return 1;
+}
+
+
+
+// === MAIN === //
 
 int main (int argc, char *argv[])
 {
@@ -142,19 +221,33 @@ int main (int argc, char *argv[])
     double tempo_pth1   = 0.0;
     double tempo_pth2   = 0.0;
 
-    // Medição Serial
+
+    // SERIAL/OPENMP/PTHREADS_1/PTHREADS_2 //
+
+    // Medição Serial //
     double inicio = pegaTempo();
     serial(imagem, largura, altura, max_iteracoes);
     double fim = pegaTempo();
     tempo_serial = fim - inicio;
-    //
 
-    // Medição OpenMP
+
+    // Medição OpenMP //
     inicio = pegaTempo();
     openmp(imagem, largura, altura, max_iteracoes, num_threads);
     fim = pegaTempo();
     tempo_openmp = fim - inicio;
-    //
+
+
+    // Medição Pthreads 1 //
+    inicio = pegaTempo();
+    if (!pthreads1(imagem, largura, altura, max_iteracoes, num_threads))
+    {
+        free(imagem);
+        return 1;
+    }
+    fim = pegaTempo();
+    tempo_pth1 = fim - inicio;
+
 
     if(!salvarImagem("mandelbrot_teste_serial.pgm", imagem, largura, altura))
     {
