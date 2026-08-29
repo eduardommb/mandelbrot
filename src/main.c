e7d7946a464602a2ea7abfdecd66725a8e9167c5
eduardommb <lemb@cesar.school>
2026-08-29 16:30:04 -0300

    feat: serial mode

--- src/main.c
+++ src/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+unsigned char calcular_pixel (int x, int y, int altura, int largura, int max_iteracoes)
+{
+    /* converte pixel p/ plano complexo */
+    double cr = -2.0 + ((double)x / largura) * 3.0;
+    double ci = -1.5 + ((double)y / altura) * 3.0;
+
+    double zr = 0.0;
+    double zi = 0.0;
+    int iter = 0;
+
+    while (iter < max_iteracoes && (zr * zr + zi * zi) <= 4.0)
+    {
+        double zr_temp = zr * zr - zi * zi + cr;
+        zi = 2.0 * zr * zi + ci;
+        zr = zr_temp;
+        iter++;
+    }
+
+    return (unsigned char)(((double)iter / max_iteracoes) * 255.0);
+}
+
+void serial(unsigned char *imagem, int largura, int altura, int max_iteracoes)
+{
+    for (int y = 0; y < altura; y++)
+    {
+        for (int x = 0; x < largura; x++)
+        {
+            imagem[y * largura + x] = calcular_pixel(x, y, largura, altura, max_iteracoes);
+        }
+    }
+}
+
+
 int main (int argc, char *argv[])
 {
     /* verifica qtd de parametros */
